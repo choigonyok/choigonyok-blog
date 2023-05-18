9d30c5ded250a961237e5312142855b20b5c73b5
최윤석 <yunsuk@cysmacbook.local>
2023-05-19 02:00:57 +0900

    develop-1

--- src/main.go
+++ src/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	_ "net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("hi")
-	http.ListenAndServe(":8080", nil)
 	eg := gin.Default()
-	eg.GET("/")
+	eg.LoadHTMLGlob("./templates/**/*.html")
+	eg.Static("/assets", "./assets")
+	//왜 images 폴더는 캐싱이 안될까?
+
+	// default homepage
+	eg.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	// redirect to homepage
+	eg.GET("/index.html", func(c *gin.Context) {
+		c.Redirect(http.StatusSeeOther, "/")
+	})
+	// eg.GET("/index.html", func(c *gin.Context) {
+	// 	c.Redirect(http.StatusSeeOther, "/")
+	// })
+	// eg.GET("/index.html", func(c *gin.Context) {
+	// 	c.Redirect(http.StatusSeeOther, "/")
+	// })
+
+	eg.Run(":8080")
 }
