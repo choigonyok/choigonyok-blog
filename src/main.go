8a5c3bf4bffcfcf377062e9d25bcab1c30b5d08c
choigonyok <achoistic98@naver.com>
2023-05-21 03:12:05 +0900

    게시판, 게시글 내용확인 구현-8

--- src/main.go
+++ src/main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -19,6 +20,13 @@ type writing struct {
 	Title        string
 	Body         string
 	Written_Time string
+	Category     string
+}
+
+type HTML_DATA struct {
+	Data_text  template.HTML
+	Data_title string
+	Data_cate  string
 }
 
 func main() {
@@ -31,7 +39,6 @@ func main() {
 	eg := gin.Default()
 	eg.LoadHTMLGlob("./templates/**/*.html")
 	eg.Static("/assets", "./assets")
-	//왜 images 폴더는 캐싱이 안될까?
 
 	// default homepage
 	eg.GET("/", func(c *gin.Context) {
@@ -57,7 +64,7 @@ func main() {
 		c.HTML(http.StatusOK, "project1.html", nil)
 	})
 
-	// DB에서 프로젝트 read
+	// DB에서 프로젝트 read해서 게시판에 올리기
 	eg.GET("/generic.html", func(c *gin.Context) {
 		st := writing{}
 		ss := []writing{}
@@ -70,13 +77,14 @@ func main() {
 			r.Scan(&t, &b, &wt)
 			st.Title = t
 			st.Body = b
+			st.Category = "projects" //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 			st.Written_Time = wt
 			ss = append(ss, st)
 		}
 		c.HTML(http.StatusOK, "generic.html", ss)
 	})
 
-	// DB에서 리뷰 read
+	// DB에서 리뷰 read해서 게시판에 올리기
 	eg.GET("/review.html", func(c *gin.Context) {
 		st := writing{}
 		ss := []writing{}
@@ -89,13 +97,14 @@ func main() {
 			r.Scan(&t, &b, &wt)
 			st.Title = t
 			st.Body = b
+			st.Category = "review" //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 			st.Written_Time = wt
 			ss = append(ss, st)
 		}
 		c.HTML(http.StatusOK, "review.html", ss)
 	})
 
-	// DB에서 스터디 read
+	// DB에서 스터디 read해서 게시판에 올리기
 	eg.GET("/study.html", func(c *gin.Context) {
 		st := writing{}
 		ss := []writing{}
@@ -108,79 +117,66 @@ func main() {
 			r.Scan(&t, &b, &wt)
 			st.Title = t
 			st.Body = b
+			st.Category = "study" //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 			st.Written_Time = wt
 			ss = append(ss, st)
 		}
 		c.HTML(http.StatusOK, "study.html", ss)
 	})
 
-	// // Write한 글을 DB에 저장
-	// eg.POST("/receive", func(c *gin.Context) {
-	// 	p_categoty := c.PostForm("post-category")
-	// 	p_title := c.PostForm("post-title")
-	// 	p_body := c.PostForm("post-write")
-	// 	// 클라이언트가 카테고리를 설정하지 않고 업로드했을 때
-	// 	if p_categoty == "" || p_title == "" || p_body == "" {
-	// 		http.Error(c.Writer, "THERE IS EMPTY BOX", http.StatusBadRequest)
-	// 	}
-	// 	// 클라이언트가 카테고리를 프로젝트로 설정하고 업로드했을 때
-	// 	if p_categoty == "projects" {
-	// 		r, err := db.Query("SELECT pid FROM projects order by pid desc limit 1")
-	// 		if err != nil {
-	// 			log.Fatalln("@@@DB connection error :", err, "@@@")
-	// 		}
-	// 		defer r.Close()
-	// 		var p_id int
-	// 		for r.Next() {
-	// 			r.Scan(&p_id)
-	// 		}
-	// 		db.Query("INSERT INTO projects values (?,?,?,?)", p_id+1, p_title, p_body, string(time.Now().Format(time.DateTime)))
-	// 		c.Redirect(http.StatusSeeOther, "/generic.html")
-	// 	}
-	// 	// 클라이언트가 카테고리를 스터디로 설정하고 업로드했을 때
-	// 	if p_categoty == "study" {
-	// 		r, err := db.Query("SELECT sid FROM study order by sid desc limit 1")
-	// 		if err != nil {
-	// 			log.Fatalln("@@@DB connection error :", err, "@@@")
-	// 		}
-	// 		defer r.Close()
-	// 		var s_id int
-	// 		for r.Next() {
-	// 			r.Scan(&s_id)
-	// 		}
-	// 		db.Query("INSERT INTO study values (?,?,?,?)", s_id+1, p_title, p_body, string(time.Now().Format(time.DateTime)))
-	// 		c.Redirect(http.StatusSeeOther, "/study.html")
-	// 	}
-	// 	// 클라이언트가 카테고리를 리뷰로 설정하고 업로드했을 때
-	// 	if p_categoty == "review" {
-	// 		r, err := db.Query("SELECT rid FROM review order by rid desc limit 1")
-	// 		if err != nil {
-	// 			log.Fatalln("@@@DB connection error :", err, "@@@")
-	// 		}
-	// 		defer r.Close()
-	// 		var r_id int
-	// 		for r.Next() {
-	// 			r.Scan(&r_id)
-	// 		}
-	// 		db.Query("INSERT INTO review values (?,?,?,?)", r_id+1, p_title, p_body, string(time.Now().Format(time.DateTime)))
-	// 		c.Redirect(http.StatusSeeOther, "/review.html")
-	// 	}
-	// })
+	// index, category 기반으로 DB에서 글 읽어와서 내용 보여주기
+	eg.GET("/lookpage", func(c *gin.Context) {
+		index := c.Query("index")
+		cate := c.Query("cate")
+		id, err := strconv.Atoi(index)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		id = id + 1
 
-	// Write한 글을 DB에 저장
+		var uid string
+		switch cate {
+		case "projects":
+			uid = "pid"
+		case "review":
+			uid = "rid"
+		case "study":
+			uid = "sid"
+		}
+
+		query := "SELECT title, body FROM " + cate + " order by " + uid + " desc limit " + strconv.Itoa(id)
+		r, err := db.Query(query)
+		if err != nil {
+			log.Fatalln("THRER IS NO POST : ", err)
+		}
+		var b, t string
+		for r.Next() {
+			r.Scan(&t, &b)
+		}
+		//HTML로 보낼 data를 담는 구조체의 필드를 설정
+		body_data1 := HTML_DATA{
+			Data_text:  template.HTML(b),
+			Data_title: t,
+			Data_cate:  cate,
+		}
+
+		c.HTML(http.StatusOK, "project1.html", body_data1)
+	})
+
+	// Write한 글을 DB에 저장하고 쓴 게시물로 이동
 	eg.POST("/markdown", func(c *gin.Context) {
 		// 요청 데이터 가져오기
 		fc := c.Request.FormValue("Cate")
 		ftt := c.Request.FormValue("Title")
 		ft := c.Request.FormValue("Text")
+
 		ft = strings.ReplaceAll(ft, "\r\n", `\r\n`)
 		data := struct {
 			Text string `json:"text"`
 		}{
 			Text: ft,
 		}
-
-		// 외부 API에 POST 요청 보내기
+		// 외부 API에 POST 요청 보내기 - 본문관련
 		url := "https://api.github.com/markdown"
 		jsonStr := []byte(fmt.Sprintf(`{"text": "%s"}`, data.Text))
 		resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonStr))
@@ -189,29 +185,26 @@ func main() {
 			return
 		}
 		defer resp.Body.Close()
-
 		// 응답 데이터 읽기
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read response from external API"})
 			return
 		}
-		fmt.Println("@@@@@@@@", string(body), "@@@@@@@@")
-
-		// if err := c.ShouldBindJSON(&data); err != nil {
-		// 	c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request data"})
-		// 	return
-		// }
-
-		fmt.Println("json text :", data.Text)
-		fmt.Println("json title :", ftt)
-		fmt.Println("json cate :", fc)
+		//HTML로 보낼 data를 담는 구조체의 필드를 설정
+		body_data := HTML_DATA{
+			Data_text:  template.HTML(string(body)),
+			Data_title: ftt,
+			Data_cate:  fc,
+		}
+		//
+		//
+		//
 
 		// 클라이언트가 카테고리를 설정하지 않고 업로드했을 때
 		if fc == "" || ft == "" || ftt == "" {
 			http.Error(c.Writer, "THERE IS EMPTY BOX", http.StatusBadRequest)
 		}
-
 		// 클라이언트가 카테고리를 프로젝트로 설정하고 업로드했을 때
 		if fc == "projects" {
 			r, err := db.Query("SELECT pid FROM projects order by pid desc limit 1")
@@ -225,17 +218,8 @@ func main() {
 			}
 			db.Query("INSERT INTO projects values (?,?,?,?)", p_id+1, ftt, data.Text, string(time.Now().Format(time.DateTime)))
 
-			fmt.Println(string(body))
-			data := struct {
-				TagHTML template.HTML
-			}{
-				TagHTML: template.HTML(string(body)),
-			}
-			c.HTML(http.StatusOK, "project1.html", data)
-
-			//escapedHTML := template.HTMLEscapeString(string(body))
-
-			// c.Redirect(http.StatusSeeOther, "/generic.html")
+			// 깃허브 마크다운 API 결과값 + 제목 + 카테고리 data를 project1.html로 전송
+			c.HTML(http.StatusOK, "project1.html", body_data)
 		}
 		// 클라이언트가 카테고리를 스터디로 설정하고 업로드했을 때
 		if fc == "study" {
@@ -249,7 +233,9 @@ func main() {
 				r.Scan(&s_id)
 			}
 			db.Query("INSERT INTO study values (?,?,?,?)", s_id+1, ftt, data.Text, string(time.Now().Format(time.DateTime)))
-			c.Redirect(http.StatusSeeOther, "/study.html")
+
+			// 깃허브 마크다운 API 결과값 + 제목 + 카테고리 data를 project1.html로 전송
+			c.HTML(http.StatusOK, "project1.html", body_data)
 		}
 		// 클라이언트가 카테고리를 리뷰로 설정하고 업로드했을 때
 		if fc == "review" {
@@ -263,139 +249,11 @@ func main() {
 				r.Scan(&r_id)
 			}
 			db.Query("INSERT INTO review values (?,?,?,?)", r_id+1, ftt, data.Text, string(time.Now().Format(time.DateTime)))
-			c.Redirect(http.StatusSeeOther, "/review.html")
+
+			// 깃허브 마크다운 API 결과값 + 제목 + 카테고리 data를 project1.html로 전송
+			c.HTML(http.StatusOK, "project1.html", body_data)
 		}
 	})
-
-	// text := c.PostForm(`post-write`)
-	// apiURL := "https://api.github.com/markdown"
-	// payload := fmt.Sprintf(`{"text": "%s"}`, text)
-	// fmt.Println(payload)
-	// req, err := http.NewRequest("POST", apiURL, strings.NewReader(payload))
-	// if err != nil {
-	// 	fmt.Println("Failed to create request:", err)
-	// 	return
-	// } //strings.NewReader(payload)
-
-	// req.Header.Set("Content-Type", "application/json")
-	// req.Header.Set("Accept", "text/html")
-	// req.Header.Set("Authorization", "Bearer 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")
-	// req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
-
-	// client := http.Client{}
-	// resp, err := client.Do(req)
-	// if err != nil {
-	// 	fmt.Println("API request failed:", err)
-	// 	return
-	// }
-	// defer resp.Body.Close()
-
-	// if resp.StatusCode != http.StatusOK {
-	// 	fmt.Println("API request failed with status:", resp.StatusCode)
-	// 	return
-	// }
-
-	// respBody, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	fmt.Println("Json EDEEEERROR3333")
-	// 	return
-	// }
-	// fmt.Println("HTML Response:")
-	// fmt.Println(string(respBody))
-
-	// c.HTML(http.StatusOK, "project1.html", string(respBody))
-
-	// decodedResult := html.UnescapeString(response.Result)
-	// c.HTML(http.StatusOK, "project1.html", decodedResult)
-	//	result := c.GetString("result")
-
-	// c.HTML(http.StatusOK, "project1.html", result)
-
-	// 	p_categoty := c.PostForm("post-category")
-	// 	p_title := c.PostForm("post-title")
-	// 	p_body := c.PostForm("post-write")
-	// 	// 클라이언트가 카테고리를 설정하지 않고 업로드했을 때
-	// 	if p_categoty == "" || p_title == "" || p_body == "" {
-	// 		http.Error(c.Writer, "THERE IS EMPTY BOX", http.StatusBadRequest)
-	// 	}
-	// 	// 클라이언트가 카테고리를 프로젝트로 설정하고 업로드했을 때
-	// 	if p_categoty == "projects" {
-	// 		r, err := db.Query("SELECT pid FROM projects order by pid desc limit 1")
-	// 		if err != nil {
-	// 			log.Fatalln("@@@DB connection error :", err, "@@@")
-	// 		}
-	// 		defer r.Close()
-	// 		var p_id int
-	// 		for r.Next() {
-	// 			r.Scan(&p_id)
-	// 		}
-	// 		db.Query("INSERT INTO projects values (?,?,?,?)", p_id+1, p_title, resp.Body, string(time.Now().Format(time.DateTime)))
-	// 		c.Redirect(http.StatusSeeOther, "/generic.html")
-	// 	}
-	// 	// 클라이언트가 카테고리를 스터디로 설정하고 업로드했을 때
-	// 	if p_categoty == "study" {
-	// 		r, err := db.Query("SELECT sid FROM study order by sid desc limit 1")
-	// 		if err != nil {
-	// 			log.Fatalln("@@@DB connection error :", err, "@@@")
-	// 		}
-	// 		defer r.Close()
-	// 		var s_id int
-	// 		for r.Next() {
-	// 			r.Scan(&s_id)
-	// 		}
-	// 		db.Query("INSERT INTO study values (?,?,?,?)", s_id+1, p_title, resp.Body, string(time.Now().Format(time.DateTime)))
-	// 		c.Redirect(http.StatusSeeOther, "/study.html")
-	// 	}
-	// 	// 클라이언트가 카테고리를 리뷰로 설정하고 업로드했을 때
-	// 	if p_categoty == "review" {
-	// 		r, err := db.Query("SELECT rid FROM review order by rid desc limit 1")
-	// 		if err != nil {
-	// 			log.Fatalln("@@@DB connection error :", err, "@@@")
-	// 		}
-	// 		defer r.Close()
-	// 		var r_id int
-	// 		for r.Next() {
-	// 			r.Scan(&r_id)
-	// 		}
-	// 		db.Query("INSERT INTO review values (?,?,?,?)", r_id+1, p_title, resp.Body, string(time.Now().Format(time.DateTime)))
-	// 		c.Redirect(http.StatusSeeOther, "/review.html")
-	// 	}
-	// })
-
-	// eg.POST("/markdown", func(c *gin.Context) {
-	// 	text := c.PostForm("text")
-	// 	apiURL := "https://api.github.com/markdown"
-	// 	payload := []byte(fmt.Sprintf(`{"text": "%s"}`, text))
-	// 	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(payload))
-	// 	if err != nil {
-	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-	// 	req.Header.Set("Accept", "application/vnd.github+json")
-	// 	req.Header.Set("Authorization", "Bearer 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")
-	// 	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
-
-	// 	client := http.Client{}
-	// 	resp, err := client.Do(req)
-	// 	if err != nil {
-	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-	// 	defer resp.Body.Close()
-
-	// 	respBody, err := ioutil.ReadAll(resp.Body)
-	// 	if err != nil {
-	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-	// 	c.HTML(http.StatusOK, "project1.html", respBody)
-
-	// 	// 응답 반환
-	// 	c.JSON(http.StatusOK, gin.H{
-	// 		"message": "Success",
-	// 		"result":  string(respBody),
-	// 	})
-	// })
 
 	eg.Run(":8080")
 
